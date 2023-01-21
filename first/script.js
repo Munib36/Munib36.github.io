e41409f2f9823db332b1f023f84a7d935d8832e1
Munib36 <mr.munib@mail.ru>
2023-01-21 10:28:17 +0300

    fix

--- first/script.js
+++ first/script.js
@@ -1,10 +1,29 @@
 const input = document.getElementById("input")
 const DOM = document.getElementById("DOM");
+const DOM2 = document.getElementById("DOM2");
 const SUBMIT = document.getElementById("submit");
-const total = document.getElementById("total");
+let i = 0;
+let array = [];
 let a = 0;
 
+
+    SUBMIT.onclick = function() {
+        array[i] = Number(input.value);
+        i++;
+
+        for (let j = 0; j < array.length; j++) {
+             a += array[j]
+             console.log("ahmad")
+        }
+        DOM.innerHTML = a;
+        a = 0;
+        DOM2.innerHTML = array;
+    }
+
+
+/*
 SUBMIT.onclick = function() {
     a = Number(a + Number(input.value)) ;
     DOM.innerText = a;
 }
+*/
