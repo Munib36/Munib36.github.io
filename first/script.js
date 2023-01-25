d2fa3f043d52ed0db96a9539bd55407dd6376a9f
Munib <mr.munib@mail.ru>
2023-01-25 12:34:49 +0300

    git commit test
    this is me just learning git bash ok?

--- first/script.js
+++ first/script.js
@@ -1,13 +1,11 @@
 const INPUT1 = document.getElementById("input1")
-const INPUT2 = document.getElementById("input2")
-const INPUT3 = document.getElementById("input3")
-const INPUT4 = document.getElementById("input4")
 const DOM = document.getElementById("DOM");
 const SUBMIT = document.getElementById("submit");
+
 SUBMIT.onclick = function() {
-    let x1 = INPUT1.value;
-    let x2 = INPUT2.value;
-    let y1 = INPUT3.value;
-    let y2 = INPUT4.value;
-    DOM.innerHTML = ((y2-y1)/(x2-x1));
+    for(let i = 0; i <= INPUT1.value; i++) {
+        for(let j = 0; j <= i; j++){
+            console.log("*")
+        }
+    }
 }
