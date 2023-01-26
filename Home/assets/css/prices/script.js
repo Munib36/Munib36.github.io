ce1defcb89330032e108204431e5ac82fa4c9d2a
Munib <mr.munib@mail.ru>
2023-01-26 12:54:25 +0300

    update

---
+++ Home/assets/css/prices/script.js
@@ -0,0 +1,20 @@
+document.getElementById("buttons-js").onclick = function() {
+    document.getElementById("buttons-js").style.opacity = "1";
+    document.getElementById("buttons-js1").style.opacity = "0.7";
+    document.getElementById("buttons-js2").style.opacity = "0.7";
+    console.log("ahmad")
+}
+document.getElementById("buttons-js1").onclick = function() {
+    document.getElementById("buttons-js").style.opacity = "0.7";
+    document.getElementById("buttons-js1").style.opacity = "1";
+    document.getElementById("buttons-js2").style.opacity = "0.7";
+    console.log("ahmad")
+}
+document.getElementById("buttons-js2").onclick = function() {
+    document.getElementById("buttons-js").style.opacity = "0.7";
+    document.getElementById("buttons-js1").style.opacity = "0.7";
+    document.getElementById("buttons-js2").style.opacity = "1";
+
+    
+    console.log("ahmad")
+}
