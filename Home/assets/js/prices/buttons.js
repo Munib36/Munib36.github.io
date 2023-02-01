3fbeaf9aeb796753e6b4dfa156661e3075c7bc88
Munib <mr.munib@mail.ru>
2023-02-01 09:32:27 +0300

    centered contact

--- Home/assets/js/prices/buttons.js
+++ Home/assets/js/prices/buttons.js
@@ -1,16 +1,16 @@
-document.getElementById("buttons-js").onclick = function() {
+document.getElementById("first").onclick = function() {
     document.getElementById("buttons-js").style.opacity = "1";
     document.getElementById("buttons-js1").style.opacity = "0.7";
     document.getElementById("buttons-js2").style.opacity = "0.7";
     console.log("ahmad")
 }
-document.getElementById("buttons-js1").onclick = function() {
+document.getElementById("second").onclick = function() {
     document.getElementById("buttons-js").style.opacity = "0.7";
     document.getElementById("buttons-js1").style.opacity = "1";
     document.getElementById("buttons-js2").style.opacity = "0.7";
     console.log("ahmad")
 }
-document.getElementById("buttons-js2").onclick = function() {
+document.getElementById("third").onclick = function() {
     document.getElementById("buttons-js").style.opacity = "0.7";
     document.getElementById("buttons-js1").style.opacity = "0.7";
     document.getElementById("buttons-js2").style.opacity = "1";
