842c49e4ffdd98ad7770f4255252245adaaec67b
Munib <mr.munib@mail.ru>
2023-02-05 11:09:23 +0300

    buttons work

--- first/script.js
+++ first/script.js
@@ -1,11 +1,4 @@
-const INPUT1 = document.getElementById("input1")
-const DOM = document.getElementById("DOM");
-const SUBMIT = document.getElementById("submit");
-
-SUBMIT.onclick = function() {
-    for(let i = 0; i <= INPUT1.value; i++) {
-        for(let j = 0; j <= i; j++){
-            console.log("*")
-        }
-    }
-}
+const img = document.querySelector('img')
+img.ondragstart = () => {
+  return false;
+};
