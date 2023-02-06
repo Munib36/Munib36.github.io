37b5a8b5f870a6be48049cdc157c4d19e70ce2d7
Munib <mr.munib@mail.ru>
2023-02-06 11:56:16 +0300

    e

--- Home/assets/js/prices/buttons.js
+++ Home/assets/js/prices/buttons.js
@@ -12,6 +12,8 @@ document.getElementById("first").onclick = function() {
     document.getElementById("length-selector-full-body").style.display = "block";
     document.getElementById("length-selector-back").style.display = "none";
     document.getElementById("length-selector-other").style.display = "none";
+
+    document.getElementById("cards").style.display = "inline-block";
 }
 document.getElementById("second").onclick = function() {
     document.getElementById("buttons-js").style.opacity = "0.7";
@@ -27,13 +29,14 @@ document.getElementById("second").onclick = function() {
     document.getElementById("length-selector-full-body").style.display = "none";
     document.getElementById("length-selector-back").style.display = "block";
     document.getElementById("length-selector-other").style.display = "none";
+
+    document.getElementById("cards").style.display = "inline-block";
 }
 document.getElementById("third").onclick = function() {
     document.getElementById("buttons-js").style.opacity = "0.7";
     document.getElementById("buttons-js1").style.opacity = "0.7";
     document.getElementById("buttons-js2").style.opacity = "1";
     console.log("ahmadthird")
-    
     document.getElementById("full").style.display = "none";
     document.getElementById("back").style.display = "none";
     document.getElementById("other").style.display = "block";
@@ -43,5 +46,8 @@ document.getElementById("third").onclick = function() {
     document.getElementById("length-selector-full-body").style.display = "none";
     document.getElementById("length-selector-back").style.display = "none";
     document.getElementById("length-selector-other").style.display = "block";
+
+    document.getElementById("cards").style.display = "inline-block";
 }
+
 
