6707c7664454d2ffef4dd165b4cc104ff0bcf9c0
Munib <mr.munib@mail.ru>
2023-02-15 15:57:27 +0300

    e

--- MUNIB/script.js
+++ MUNIB/script.js
@@ -1,7 +1,7 @@
 let prices = [1, 12 ,14, 6, 12, 30, 20];
 let total = 0;
-
-for (let i = 0; i <= prices.length; i++) {
-	total += prices[i];
+let i = 0;
+for(i = 0; i <= prices.length; i++){
+	total + prices[i];
 }
 console.log(total);
