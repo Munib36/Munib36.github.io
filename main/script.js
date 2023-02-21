720310bff9d94fe89b459b53ab3740d1257c7ffa
Boniph36 <mrmuni1050@gmail.com>
2023-02-21 14:50:09 +0300

    e

--- main/script.js
+++ main/script.js
@@ -3,8 +3,10 @@ const label = document.getElementById("label");
 let num = 0;
 
 button.onclick = function() {
-  num = Math.floor(Math.random() * 400 + 1);
-  label.innerHTML = num;
-  let answer = Math.sqrt(num);
-  console.log(answer.toFixed(2));
+    num = Math.floor(Math.random() * 400 + 1);
+    label.innerHTML = num;
+    let answer = Math.sqrt(num);
+    console.log(answer.toFixed(2));
+    document.getElementById("hint").innerHTML = Math.floor(answer);
+	document.getElementById("hint2").innerHTML = Math.pow(Math.floor(answer), 2);
 };
