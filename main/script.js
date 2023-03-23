9389395029b5aefbc9b2f3bc29d8cf2b32cd5ce0
Munib36 <mr.munib@mail.ru>
2023-03-23 13:02:02 +0300

    vv

--- main/script.js
+++ main/script.js
@@ -1,12 +1,24 @@
-const button = document.getElementById("enter");
-const label = document.getElementById("label");
-let num = 0;
+class Car{
+    constructor(name, speed, color){
+        this.name = name;
+        this.speed = speed;
+        this.color = color;
+    }
+    drive(){
+        console.log(`you drive the ${this.name}`)
+    }
+}
 
-button.onclick = function() {
-    num = Math.floor(Math.random() * 400 + 1);
-    label.innerHTML = num;
-    let answer = Math.sqrt(num);
-    console.log(answer.toFixed(2));
-    document.getElementById("hint").innerHTML = Math.floor(answer);
-	document.getElementById("hint2").innerHTML = Math.pow(Math.floor(answer), 2);
-};
+const car1 = new Car("lambo", 220, "green")
+const car2 = new Car("fiat", 40, "white")
+const car3 = new Car("challenger", 90, "red")
+const car4 = new Car("hyundai", 70, "black")
+
+const cars = [car1, car2, car3, car4]
+
+function startrace(cars) {
+
+}
+for(const car of cars){
+    car.drive()
+}
