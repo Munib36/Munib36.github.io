876184a5bf38d79a57d2758e2edfd5e53e60ebe3
Munib36 <mr.munib@mail.ru>
2023-11-07 17:01:15 +0300

    .md test

--- my travel journal/src/assets/journals.js
+++ my travel journal/src/assets/journals.js
@@ -7,7 +7,7 @@ export default [
         place: "Kaaba",
         startDate: "5 Jan, 2010",
         endDate: "7 Jan, 2010",
-        desc: "Kaaba is a center point of islam, brings all muslims to a specific place, it stood for 1400+ years, Millions of people come here to perform Pilgrimage, it holds history and shows detication of muslims from the past and present.",
+        desc: "The Kaaba, a revered black cube-like structure, stands tall and majestic, drawing pilgrims from every corner of the globe like a magnet. It serves as the focal point of Islam and a symbol of unity, surrounded by a diverse sea of worshipers. It's important to note that Muslims don't worship the Kaaba itself; it simply serves as a direction for their prayers. This sacred site creates a profound sense of awe for anyone fortunate enough to encounter it.",
         // done
     },
     {
@@ -18,7 +18,7 @@ export default [
         place: "Red square",
         startDate: "15 Oct, 2019",
         endDate: "22 Oct, 2019",
-        desc: "Red Square has been the scene of executions, demonstrations, riots, parades, and speeches. Almost 73,000 square metres",
+        desc: "In the heart of Moscow, the Red Square captivates with its rich history. Surrounded by landmarks like the Kremlin, it's a vibrant hub where Russia's past and present meet. The vibrant facades and cultural heritage make it a must-visit, inviting everyone into a world of captivating stories and wonders.",
         // done
     },
     {
@@ -29,18 +29,8 @@ export default [
         place: "Roman temple of Hercules",
         startDate: "13 Mar, 2021",
         endDate: "20 Mar, 2021",
-        desc: "Temple of Hercules is a historic site in the Amman Citadel in Amman, Jordan. It is thought to be the most significant Roman structure in the Amman Citadel.",
+        desc: "In Amman, Jordan, the Roman Temple of Hercules stands as a majestic testament to ancient times. Amidst the cityscape, it echoes the grandeur of Rome, inviting visitors to delve into history. The weathered columns and timeless aura transport you to an era of myths and legends, making it a captivating destination for history enthusiasts.",
         // done
     },
-    {
-        id: 4,
-        imageLink: "../images/kaaba1.jpg",
-        country: "Canada",
-        mapsLink: "https://www.google.com/maps/d/viewer?mid=1D16ICyhNnv-C8l8rQ_JH2755Icg&hl=en_US&ll=31.953744986778176%2C35.93481333149247&z=19",
-        place: "Niagara falls",
-        startDate: "13 Mar, 2021",
-        endDate: "20 Mar, 2021",
-        desc: "wow very halal waterfall wow very halal waterfall wow very halal waterfall wow very halal waterfall wow very halal waterfall wow very halal waterfallwow very halal waterfall wow very halal waterfall wow very halal waterfall",
-        // done
-    },
+
 ]
