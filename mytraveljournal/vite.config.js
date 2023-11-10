3c82026b7d50be872d3783a7ddde169d2238b6f5
Munib36 <mr.munib@mail.ru>
2023-11-10 06:40:05 +0300

    qwe

--- mytraveljournal/vite.config.js
+++ mytraveljournal/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://Munib36.github.io/Munib36/mytraveljournal',
+  base: '/mytraveljournal/',
   plugins: [react()],
 })
