e12f56d983bec7a0adb093265e956b38765a9eb3
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-01-13 23:04:54 -0500

    LETS GO FINALLY TENZIES KINDA WORKS
    THE RANDOM NUMBER GENERATOR WORKS AND IT RENDERS THE BUTTONS WITH THE NUMBER LETS GOOO

--- scrimba/src/App.jsx
+++ scrimba/src/App.jsx
@@ -1,74 +1,48 @@
-
 import Die from "./components/Die"
 import "./app.css"
-import { useState } from "react";
-
-const [ diceArray, setDiceArray ] = useState([
-	{on: false, value: 1},
-	{on: false, value: 1},
-	{on: false, value: 1},
-	{on: false, value: 1},
-	{on: false, value: 1},
+import React, {
+	useState,
+} from 'react';
+function ranNum() {
+	return Math.ceil(Math.random() * 6)
 	
-	{on: false, value: 1},
-	{on: false, value: 1},
-	{on: false, value: 1},
-	{on: false, value: 1},
-	{on: false, value: 1},
-])
-
-
-function Title(){
-  return(
-      <div className="Title">
-          <h2>Tenzies</h2>
-          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls. </p>
-      </div>
-  )
 }
 
-function Dice(){
-  return(
-      <div className="Dice">
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
 
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-      </div>
-  )
+function Title() {
+	return ( 
+		<div className = "Title" >
+			<h2 > Tenzies </h2> 
+			<p> 
+				Roll until all dice are the same.Click each die to freeze it at its current value between rolls. 
+			</p> 
+		</div>
+	)
+}
+
+function Dice(props) {
+	let dices = props.diceArray.map(x => {
+		return(
+			<button className={x.on === true ? "active" : ""}>{x.value}</button>
+		)
+	})
+	return ( 
+		<div className="Dice" >
+			{dices}
+		</div>
+	)
 }
 
 
 
-function Button(){
-  return(
-    <div className="Button">
-      <button onClick={allNewDice}>Roll</button>
-    </div>
-  )
+function Button(props) {
+	return ( 
+		<div className = "Button" >
+			<button 
+			onClick = {props.handleClick}> Roll </button> 
+		</div>
+	)
 }
-
-function ranNum(){
-	return Math.ceil(Math.random()*6)
-}
-
-function allNewDice(){
-
-	for(let i = 0; i < 10; i++){
-		
-		diceArray[i].value = ranNum();
-	}
-	
-	console.log(diceArray)
-}
-
 
 
 
@@ -76,11 +50,45 @@ function allNewDice(){
 
 
 export default function App() {
-  return (
-      <main>  
-          <Title />
-          <Dice />
-          <Button />
-      </main>
-  )
+	const [diceArray, setDiceArray] = useState([
+		{on: true, value: 1},
+		{on: false, value: 1},
+		{on: false, value: 1},
+		{on: false, value: 1},
+		{on: false, value: 1},
+		
+		{on: false, value: 1},
+		{on: false, value: 1},
+		{on: false, value: 1},
+		{on: false, value: 1},
+		{on: false, value: 1}
+	])
+
+
+	function allNewDice() {	
+		setDiceArray(prevDice => {
+			return prevDice.map(x => { 
+				if(x.on){
+					return( {...x})
+				}else{
+					return( {...x, value: ranNum()} )
+				}
+
+			})
+		})
+		console.log(diceArray)
+	}
+
+
+	return ( 
+		<main>
+			<Title />
+			<Dice 
+				diceArray={diceArray}
+			/>
+			<Button 
+				handleClick={allNewDice}
+			/>
+		</main>
+	)
 }
