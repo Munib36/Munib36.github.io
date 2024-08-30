29a5d57027437343f3114f307c52f1b6780c16ee
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-08-30 11:50:27 -0400

    qwe

--- water/src/components/nav.jsx
+++ water/src/components/nav.jsx
@@ -1,9 +1,10 @@
 import "../css/nav.css"
+import logo from "/waterlogo.png"
 
 export default function Nav(){
     return(
         <nav>
-
+            <img src={logo} alt="" />
         </nav>
     )
 }
